d9826ddeab2eaf8a08b9392ca126d91a33071e2f
Ryan P <rdpinkstaff@gmail.com>
2023-07-21 18:52:05 -0600

    Working of calculation function

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-// day, month, year
-function calcStats() {
+
+function calcStats(day, month, year) {
     // Get current date
     const currentDate = new Date();
 
     // Getting day, month and year from currentDate
     const currentDay = currentDate.getDate();
-    const currentMonth = currentDate.getMonth();
+    const currentMonth = currentDate.getMonth() + 1;
     const currentYear = currentDate.getFullYear();
+
+    let diffMonth, diffYear, diffDay;
     
-    console.log(currentYear);
+    if(month < currentMonth) {
+        diffMonth = currentMonth - month;
+        diffYear = currentYear - year;
+    } else {
+        diffMonth = (12 - month) + currentMonth;
+        diffYear = currentYear - year - 1;
+    }
+
+    // diffDay = currentDay - day
+    
+    
 }
-calcStats();
+
 
 const button = document.getElementById("submit");
 
 button.addEventListener("click", event => {
     event.preventDefault();
-    console.log("Clicked")
+
+    // Get input values;
+    let month = document.getElementById("month").value;
+    let day = document.getElementById("day").value;
+    let year = document.getElementById("year").value;
+
+    const ageObj = calcStats(day, month, year);
+
+    
+    console.log(month);
 })
